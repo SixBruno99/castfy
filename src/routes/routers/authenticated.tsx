d6f495fc9ab590aff23f025ec5cb6c7c2e60071f
Bruno Henrique <brunohc2016@gmail.com>
2024-03-19 14:18:38 -0300

    fix: fixing authentication flow

--- src/routes/routers/authenticated.tsx
+++ src/routes/routers/authenticated.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { MainLayout } from "../../core/layouts/main";
+import { Home } from "../../pages/main/home";
 
 export function AuthenticatedRouter() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
       </Route>
     </Routes>
   );
