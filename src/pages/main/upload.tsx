387218dde1e15e0c10edbead847beae4dba8293d
Breno Duarte <brenoduarte@nasajon.com.br>
2024-08-28 13:55:21 -0300

    feat: Upload's page updated

--- src/pages/main/upload.tsx
+++ src/pages/main/upload.tsx
@@ -1,113 +1,227 @@
 import {
-    Text,
-    Box,
-    Divider,
-    Input,
-    Button,
-    Textarea,
-    VStack,
-    HStack,
-    Flex,
-    flexbox,
-  } from "@chakra-ui/react";
+  Text,
+  Box,
+  Input,
+  Button,
+  Textarea,
+  VStack,
+  HStack,
+  Flex,
+  Image
+} from "@chakra-ui/react";
+import { useState, useRef } from "react";
 
-  import { SetStateAction, useState, useRef } from "react";
-  
-  export function Upload() {
-    const inputRef = useRef();
+export function Upload() {
+  // Referência para o input de arquivo de áudio
+  const audioInputRefer = useRef<HTMLInputElement>(null);
+  // Referência para o input de imagem
+  const imageInputRef = useRef<HTMLInputElement>(null);
 
-    const [audioFile, setAudioFile] = useState(null);
-    const [description, setDescription] = useState("");
-  
-    const handleFileChange = (event: { target: { files: SetStateAction<null>[]; }; }) => {
-      setAudioFile(event.target.files[0]);
-    };
-  
-    const handleUpload = () => {
-    
-    };
-  
-    return (
-      <Box
-        display="flex"
-        flexDirection="column"
+  const [audioFile, setAudioFile] = useState<File | null>(null); // Estado para armazenar a URL do áudio
+  const [imageFile, setImageFile] = useState<File | null>(null); // Estado para armazenar a URL da imagem
+  const [audioPreview, setAudioPreview] = useState<string | null>(null); // Estado para armazenar a URL da pré-visualização do áudio
+  const [imagePreview, setImagePreview] = useState<string | null>(null); // Estado para armazenar a URL da pré-visualização
+  const [description, setDescription] = useState("");
+  const [titulo, setTitulo] = useState("");
+
+  const handleAudioFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setAudioFile(file); // Save the selected file
+      setAudioPreview(URL.createObjectURL(file)); // Cria uma URL temporária para o áudio selecionado
+    }
+  };
+
+  const handleImageFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setImageFile(file); // Salva o arquivo de imagem selecionado
+      setImagePreview(URL.createObjectURL(file)); // Cria uma URL temporária para a imagem selecionada
+    }
+  };
+
+  const handleAudioButtonClick = () => {
+    if (audioInputRefer.current) {
+      audioInputRefer.current.click(); // Trigger the click event on the hidden input
+    }
+  };
+
+  const handleImageButtonClick = () => {
+    if (imageInputRef.current) {
+      imageInputRef.current.click(); // Dispara o clique no input de imagem oculto
+    }
+  };
+
+  const truncateFileName = (fileName: string, maxLength: number) => {
+    if (fileName.length > maxLength) {
+      return fileName.slice(0, maxLength - 3) + "...";
+    }
+    return fileName;
+  };
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      width="100vw"
+      backgroundColor="#1f1f1f"
+      padding={{ base: "12px", md: "24px" }}
+      minHeight={{ base: "calc(100vh - 64px)", md: "100vh" }}
+      overflowX="hidden"
+    >
+      <VStack
+        spacing={0}
         alignItems="center"
-        justifyContent="center"
-        width="100vw"
-        backgroundColor="#1f1f1f"
-        padding={{ base: "12px", md: "24px" }}
-        minHeight={{ base: "calc(100vh - 64px)", md: "100vh" }}
-        overflowX="hidden"
-      >       
-        <VStack
-          spacing={0}
-          alignItems="start"
-          width={{ base: "100%", md: "65%" }}
-          margin="0 auto"
+        width={{ base: "100%", md: "65%" }}
+        margin="0 auto"
+      >
+        <Text
+          fontSize={{ base: "24px", md: "36px" }}
+          fontWeight="bold"
+          color="white"
+        >
+          Envie seu Podcast
+        </Text>
+
+        <Text marginBottom={8} fontSize={{ base: "14px", md: "16px" }} color="white">
+          Faça o upload do seu podcast para milhares de pessoas ouvirem.
+        </Text>
+
+        {/* Input de arquivo de áudio oculto */}
+        <input
+          type="file"
+          accept="audio/*"
+          style={{ display: "none" }}
+          ref={audioInputRefer}
+          onChange={handleAudioFileChange}
+        />
+
+        {/* Input de imagem oculto */}
+        <input
+          type="file"
+          accept="image/*"
+          ref={imageInputRef}
+          style={{ display: "none" }}
+          onChange={handleImageFileChange}
+        />
+
+
+        <Flex
+          direction={{ base: "column", "2xl": "column" }}
+          marginBottom={8}
+          alignItems={{base: "center"}}
+          justifyContent="start"
+          width={{ base: "100%", md: "100%" }}
+          gap={4}
+        >
+          <Textarea
+            id="titulo"
+            flex={3}
+            minH={45}
+            maxH={45}
+            placeholder="Digite um título para o seu podcast..."
+            value={titulo}
+            onChange={(e) => setTitulo(e.target.value)}
+            backgroundColor="white"
+            color="black"
+            resize="none"
+            height="35px"
+            width="full"
+          />
+
+          <Textarea
+            id="campoTexto"
+            flex={3}
+            minH={200}
+            placeholder="Digite uma descrição para o seu podcast..."
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            backgroundColor="white"
+            color="black"
+            resize="none"
+            height="150px"
+            width="full"
+          />
+
+        
+
+
+          
+
+          <Flex
+          direction={{base: "column", sm: "row"}}
+          alignItems={{base: "center", sm: "end"}}
+          gap={4}
+          flex={2}
+          >
+          <VStack
+          alignItems={"center"}
+            spacing={2}
             >
-            <Text
-                fontSize={{ base: "24px", md: "36px" }}
-                fontWeight="bold"
-                color="white"
-                >
-                Envie seu Podcast
-            </Text>
-
-      
-
-            <Text
-                marginBottom={8}
-                fontSize={{ base: "14px", md: "16px" }}
-                color="white"
-                >
-                Faça o upload do seu podcast para milhares de pessoas ouvirem.
-            </Text>
-
-
-            <input type="file" accept="audio/*" style={{display: "none"}}/>
-            <HStack
-                marginBottom={8}
-                display={"flex"}
-                flexDirection={"row"}
-                alignItems="center"
-                justifyContent="start"
-                width={{ base: "100%", md: "100%" }}
-                
-                >
-                <Button
-                    flex={1}
-                    color="white"
-                    backgroundColor="#004aad"
-                    colorScheme="#004aad"
-                    width="full"
-                    >
-                    Selecionar Arquivo
-                </Button>
-
-                <Textarea
-                   flex={2}
-                   placeholder="Digite uma breve descrição do áudio"
-                   value={description}
-                   onChange={(e) => setDescription(e.target.value)}
-                   backgroundColor="white"
-                   color="black"
-                   resize="none"
-                   height="150px"
-                />
-
-            </HStack>
-
+            {/* Exibe a pré-visualização da imagem se houver uma */}
+            {imagePreview && (
+              <Image
+                src={imagePreview}
+                alt="Preview da Imagem"
+                boxSize="100px"
+                objectFit="cover"
+                borderRadius="md"
+                display={{base: "block", lg: "block" }}
+              />
+            )}
             <Button
-               color="white"
-               backgroundColor="#004aad"
-               colorScheme="#004aad"
-               width="full"
-
-                >
-               Fazer Upload
+              id="botaoImagem"
+              color="white"
+              backgroundColor="#004aad"
+              colorScheme="blue"
+              width="full"
+              onClick={handleImageButtonClick} // Ação de clique no botão de imagem
+            >
+              {imageFile ? truncateFileName(imageFile.name, 20) : "Imagem de Capa"} {/* Nome da imagem ou texto padrão */}
             </Button>
-        </VStack>
-      </Box>
-    );
-  }
-  
+          </VStack>
+
+
+          <VStack
+            spacing={2}
+           >
+            {/* Exibe a pré-visualização do áudio se houver um */}
+            {audioPreview && (
+              <audio
+                controls
+                src={audioPreview}
+                style={{ marginBottom: "10px", maxWidth: "220px" }}
+              >
+                Seu navegador não suporta o elemento de áudio.
+              </audio>
+            )}
+            <Button
+              minH={35}
+              id="botaoAudio"
+              color="white"
+              backgroundColor="#004aad"
+              colorScheme="blue"
+              width="full"
+              onClick={handleAudioButtonClick}
+            >
+              {audioFile ? truncateFileName(audioFile.name, 20) : "Arquivo de Áudio"} {/* Nome do arquivo ou texto padrão */}
+            </Button>
+          </VStack>
+          </Flex>
+        </Flex>
+
+        <Button
+          id="botaoUpload"
+          color="white"
+          backgroundColor="#004aad"
+          colorScheme="blue"
+          width={{ base: "400px"}}
+        >
+          Fazer Upload
+        </Button>
+      </VStack>
+    </Box>
+  );
+}
